597fca159274bc1adc673c1c3d0808fa6a33074d
Nathan Reed <reednj@gmail.com>
2017-04-21 14:26:55 +0800

    add some other handy options

--- lib/git/make_mirror.rb
+++ lib/git/make_mirror.rb
@@ -9,19 +9,44 @@ module Git
 				opts = Trollop::options do
 					version "git-make-mirror #{Git::MakeMirror::VERSION} (c) 2017 @reednj (reednj@gmail.com)"
 					banner "Usage: git make-mirror [options] [remote]"
+					opt :remote, "create a remote in the local repository", :type => :string
+					opt :push, "push to the newly created mirror"
 				end
 
+				git_remote_name = nil
+
+				puts "creating remote repository"
 				self.create_repository
+
+				puts "copying post-receive hook"
 				self.copy_hook
+
+				if !opts[:remote].nil?
+					git_remote_name = opts[:remote]
+					sh "git remote add #{git_remote_name} #{remote_url}"
+				end
+
+				if opts[:push]
+					sh "git push #{git_remote_name || remote_url} master"
+				end
 			
+			end
+
+			def sh(cmd)
+				puts cmd
+				system cmd
 			end
 
 			def local_hooks_dir
 				@hooks_dir ||= File.join(File.dirname(__FILE__), 'hooks')
 			end
 
+			def remote_url
+				ARGV.last.strip
+			end
+
 			def remote
-				@remote ||= parse_remote ARGV.last
+				@remote ||= parse_remote(self.remote_url)
 			end
 
 			def server
@@ -30,7 +55,7 @@ module Git
 
 			def parse_remote(remote)
 				a = remote.split(':')
-				{ :host => a.first, :path => a.last }
+				{ :host => a[0], :path => a[1] }
 			end
 
 			def create_repository
