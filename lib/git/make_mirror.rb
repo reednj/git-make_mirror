9ea817afe43dbb7f51b253a885a3e4778bbf4a35
Nathan Reed <reednj@gmail.com>
2017-06-14 13:17:29 +0800

    allow configuring local repositories

--- lib/git/make_mirror.rb
+++ lib/git/make_mirror.rb
@@ -9,11 +9,17 @@ module Git
 				opts = Trollop::options do
 					version "git-make-mirror #{Git::MakeMirror::VERSION} (c) 2017 @reednj (reednj@gmail.com)"
 					banner "Usage: git make-mirror [options] [remote]"
-					opt :remote, "create a remote in the local repository", :type => :string
+					opt :remote, "create a remote with the given name in the local repository", :type => :string
+					opt :local, "configure a local repository to receive pushes", :default => false
 					opt :push, "push to the newly created mirror"
 				end
 				
-				Trollop::educate if remote_url.nil? || remote_url == ''
+				Trollop::educate if (remote_url.nil? || remote_url == '') && !opts[:local]
+
+				if opts[:local]
+					configure_repository_local
+					return
+				end
 				
 				git_remote_name = nil
 
@@ -43,6 +49,10 @@ module Git
 				@hooks_dir ||= File.join(File.dirname(__FILE__), 'hooks')
 			end
 
+			def local_hook_file
+				@local_hook_file ||= File.join(local_hooks_dir, 'post-receive.rb')
+			end
+
 			def remote_url
 				(ARGV.last || '').strip
 			end
@@ -69,11 +79,22 @@ module Git
 				]
 			end
 
+			def configure_repository_local
+				hook_file = '.git/hooks/post-receive'
+				sh 'git config receive.denyCurrentBranch ignore'
+				FileUtils.cp local_hook_file, hook_file
+				sh "chmod 775 #{hook_file}"
+			end
+
 			def copy_hook
-				local_hook_file = File.join local_hooks_dir, 'post-receive.rb'
 				remote_hook_file = File.join remote[:path], '.git/hooks/post-receive'
 				server.scp local_hook_file, remote_hook_file
 				server.exec "chmod 775 #{remote_hook_file}"
+			end
+
+			def sh(cmd)
+				$stderr.puts cmd
+				system cmd
 			end
 		end
 
